54cb8396e369b3d606b8ec5d9b2e8e972bd72748
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2025-02-06 09:04:19 -0400

    add status api

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import router from "./src/routes/auth.routes.js";
 import customerRoutes from "./src/routes/customer.routes.js";
+import statusRouter from "./src/routes/status.routes.js";
 import { config } from "./src/config/env.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/status", statusRouter);
 app.use("/api/auth", router);
 app.use("/api/customers", customerRoutes);
 
